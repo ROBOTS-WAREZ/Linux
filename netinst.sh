e2e2552dd3385101d4994311ab8a5b8553cdac2e
Shyam Nath <Sharminator@users.noreply.github.com>
2015-03-23 13:33:34 +1100

    Update netinst.sh

--- netinst.sh
+++ netinst.sh
@@ -3,8 +3,8 @@ apt-get dist-upgrade;
 
 apt-get install\
     sudo xorg openbox\
-    openjdk-7-jdk openjdk-7-jre icedtea-netx\ # Java
-    iceweasel\ # Web Browser & File Viewer
-    gimp inkscape blender\ # Graphical Editors (Rasta, Vector, 3D)
+    openjdk-7-jdk openjdk-7-jre icedtea-netx\
+    iceweasel\
+    gimp inkscape blender\
     --no-install-recommends --assume-yes\
 ;
