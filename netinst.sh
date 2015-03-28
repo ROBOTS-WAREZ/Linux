edf7d30acfec221ece98fe121d5db081cced86b8
Shyam Nath <Sharminator@users.noreply.github.com>
2015-03-28 04:45:52 +0000

    Update netinst.sh

--- netinst.sh
+++ netinst.sh
@@ -1,4 +1,11 @@
 apt-get update;
 apt-get dist-upgrade;
 
-apt-get install sudo adduser xorg openbox openjdk-7-jdk openjdk-7-jre icedtea-netx iceweasel gimp inkscape blender --no-install-recommends --assume-yes;
+apt-get install \
+    sudo adduser \
+    xorg openbox \
+    openjdk-7-jdk openjdk-7-jre icedtea-netx \
+    iceweasel \
+    gimp inkscape blender \
+    --no-install-recommends --assume-yes \
+;
