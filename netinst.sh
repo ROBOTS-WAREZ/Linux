b9e50fa7c8b280f97e3863aff4290661cddbf5f0
Shyam Nath <Sharminator@users.noreply.github.com>
2015-03-31 19:12:56 +1100

    Update netinst.sh

--- netinst.sh
+++ netinst.sh
@@ -130,7 +130,7 @@ iptables -A INPUT -p udp --sport 53 -j ACCEPT;
 iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT;
 
 # Allow outgoing signals.
-iptables -I OUTPUT 1 -m state --state RELATED,ESTABLISHED -j ACCEPT;
+iptables -I OUTPUT 1 -m state --state ESTABLISHED,RELATED -j ACCEPT;
 iptables -A OUTPUT -p tcp -m multiport --dports 22,80,443 -m state --state NEW,ESTABLISHED -j ACCEPT;
 iptables -A OUTPUT -p udp --dport 53 -m state --state NEW -j ACCEPT;
 iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT;
