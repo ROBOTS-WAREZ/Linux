3b9de871bc2caa9ced33d92e320cda356e1c08ec
Shyam Nath <Sharminator@users.noreply.github.com>
2015-10-06 01:19:49 +1100

    Update FEZ.sh

--- FEZ.sh
+++ FEZ.sh
@@ -1,16 +1,17 @@
 #Need to find a better way of doing this...
 #https://forums.mozilla.org/viewtopic.php?p=33582&sid=f87500f0267f160fb0389b26636bf131
 firefox\
-    https://addons.mozilla.org/firefox/downloads/latest/tab-for-a-cause/\
-    https://addons.mozilla.org/firefox/downloads/latest/ecosia-the-green-search/\
-    https://addons.mozilla.org/firefox/downloads/latest/ecolink/\
-    https://addons.mozilla.org/firefox/downloads/latest/calomel-ssl-validation/\
-    https://addons.mozilla.org/firefox/downloads/latest/wot-safe-browsing-tool/\
-    https://addons.mozilla.org/firefox/downloads/latest/flagfox/\
-    https://addons.mozilla.org/firefox/downloads/latest/greasemonkey/\
-    https://addons.mozilla.org/firefox/downloads/latest/stylish/\
-    https://addons.mozilla.org/firefox/downloads/latest/ublock/\
-    https://addons.mozilla.org/firefox/downloads/latest/cleantube/\
-    https://addons.mozilla.org/firefox/downloads/latest/bloody-vikings/\
-    https://addons.mozilla.org/firefox/downloads/latest/imacros-for-firefox/\
+    https://addons.mozilla.org/En-uS/firefox/addon/tab-for-a-cause/\
+    https://addons.mozilla.org/EN-uS/firefox/addon/ecosia-the-green-search/\
+    https://addons.mozilla.org/en-US/firefox/addon/ecolink/\
+    https://addons.mozilla.org/en-US/firefox/addon/calomel-ssl-validation/\
+    https://addons.mozilla.org/en-US/firefox/addon/wot-safe-browsing-tool/\
+    https://addons.mozilla.org/en-US/firefox/addon/flagfox/\
+    https://addons.mozilla.org/en-US/firefox/addon/bloody-vikings/\
+    https://addons.mozilla.org/en-US/firefox/addon/chatzilla/\
+    https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/\
+    https://addons.mozilla.org/en-US/firefox/addon/stylish/\
+    https://addons.mozilla.org/en-US/firefox/addon/ublock/\
+    https://addons.mozilla.org/en-us/firefox/addon/cleantube/\
+    https://addons.mozilla.org/en-US/firefox/addon/imacros-for-firefox/\
 ;
